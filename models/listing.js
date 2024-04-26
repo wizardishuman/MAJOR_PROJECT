51b7b991d6fe09a3b5f373b4653d6727496b4a0e
saurabh patil <saurabh.pvt.999@gmail.com>
2024-04-26 18:32:38 +0530

    2nd commitw

--- models/listing.js
+++ models/listing.js
@@ -1,3 +1,4 @@
+const { ref } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -9,15 +10,23 @@ const listingSchema = new Schema({
   description: String,
   image: {
     type: String,
+    default:
+      "https://images.unsplash.com/photo-1707080213273-5a5112431d94?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     set: (v) =>
       v === ""
         ? "https://images.unsplash.com/photo-1707080213273-5a5112431d94?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         : v,
   },
-  Price: Number,
+  price: Number,
   location: String,
   country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
-const Listing = mongoose.model("Listing ", listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
